5bc5b66c068bfb4c25350467ee45ac2f8f7eeb6e
shilpahosur25 <51725835+shilpahosur25@users.noreply.github.com>
2022-05-11 02:30:46 -0400

    Update sts.rb

--- sts.rb
+++ sts.rb
@@ -3,18 +3,15 @@ require 'csv'
 #reading csv file and setting header=true
 data=CSV.read("sample_data.csv",headers:true)
 
-
 puts ("-----------------------------------------------")
 # 1. The total number of records in the file.
 puts ("1. Total number of records = " + data.size.to_s)
-
 puts ("-----------------------------------------------")
 
 # 2. The total number of unique email addresses in the file. 
 puts ("2. Total number of unique email = " + data['email'].uniq.size.to_s)
-
-
 puts ("-----------------------------------------------")
+
 # Regex for email address
 VALID_EMAIL_REGEX = /^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/i
 
@@ -41,7 +38,6 @@ end
 
 # 3.The total number of valid email addresses in the file.\
 puts ("3. Total number of records with valid email addresses = " + valid_data.size.to_s)
-
 puts ("-----------------------------------------------")
 
 # Re-run to check unique emails with valid email addresses
@@ -55,7 +51,6 @@ valid_uniq_data = CSV.read("valid_uniq.csv")
 
 # 3.The total number of valid and unique email addresses in the file.\
 puts ("3. Total number of records with valid and unique email addresses = " + valid_uniq_data.size.to_s)
-
 puts ("-----------------------------------------------")
 
 # Lets split the email address with domain name as new column to sheet
@@ -78,5 +73,4 @@ end
 # 4. Sort by domain counts, descending order.
 puts "4. Sort by domain counts, descending order:"
 puts count_arr.sort_by{ |k, v| v }.reverse
-
 puts ("-----------------------------------------------")
